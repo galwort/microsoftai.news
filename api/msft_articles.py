c2dbbd3c28c1138d4d06a2604591299eb4f328bb
galwort <wthomasgorbett@gmail.com>
2024-05-16 09:37:19 -0400

    Refactor into function

--- api/msft_articles.py
+++ api/msft_articles.py
@@ -1,28 +1,38 @@
-import requests
 from bs4 import BeautifulSoup
+from requests import get
 
-url = "https://news.microsoft.com/source/tag/ai/"
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
-}
-response = requests.get(url, headers=headers)
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, "html.parser")
+def get_article_info():
+    url = "https://news.microsoft.com/source/tag/ai/"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
+    }
+    response = get(url, headers=headers)
 
-    articles = soup.find_all("div", class_="fwpl-row el-8cjrpw")
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    for article in articles:
-        title_element = article.find("div", class_="fwpl-item el-fz703r h2")
-        title = title_element.text.strip()
-        link = title_element.find("a")["href"]
+        articles = soup.find_all("div", class_="fwpl-row el-8cjrpw")
 
-        date_element = article.find("div", class_="fwpl-item el-37vm0k7 kicker")
-        date = date_element.text.strip()
+        for article in articles:
+            title_element = article.find("div", class_="fwpl-item el-fz703r h2")
+            title = title_element.text.strip()
+            link = title_element.find("a")["href"]
 
-        print(f"Title: {title}")
-        print(f"Link: {link}")
-        print(f"Date: {date}")
-        print()
-else:
-    print("Failed to retrieve the webpage. Status code:", response.status_code)
+            date_element = article.find("div", class_="fwpl-item el-37vm0k7 kicker")
+            date = date_element.text.strip()
+
+            print(f"Title: {title}")
+            print(f"Link: {link}")
+            print(f"Date: {date}")
+            print()
+    else:
+        print("Failed to retrieve the webpage. Status code:", response.status_code)
+
+
+def main():
+    get_article_info()
+
+
+if __name__ == "__main__":
+    main()
