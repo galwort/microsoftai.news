000efa07cecbc7480d0857c426b2012d941a5a87
galwort <wthomasgorbett@gmail.com>
2024-05-23 13:58:51 -0400

    Start implementing structure for data storage

--- ui/src/app/home/home.page.ts
+++ ui/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BlobServiceClient } from '@azure/storage-blob';
 
 @Component({
   selector: 'app-home',
@@ -32,45 +33,110 @@ export class HomePage implements OnInit, AfterViewInit {
     this.addPopoverListeners();
   }
 
-  loadArticles() {
-    this.loading = true;
-    this.http
-      .get<any[]>(`${this.apiUrl}/get_articles`)
-      .subscribe((articlesData) => {
-        this.articles = articlesData;
+  private async initializeAzureStorage(): Promise<BlobServiceClient> {
+    const connectionString = 'your-azure-storage-connection-string';
+    const blobServiceClient =
+      BlobServiceClient.fromConnectionString(connectionString);
+    return blobServiceClient;
+  }
 
-        const articlePromises = this.articles.map((article) =>
-          this.http
-            .post<any>(`${this.apiUrl}/gen_summaries`, {
-              title: article.title,
-              content: article.content,
-            })
-            .toPromise()
-            .then((summaryResponse) => {
-              article.slug = summaryResponse.slug;
-              article.summary = summaryResponse.summary;
-              return article;
-            })
-        );
+  async scrapeArticles() {
+    try {
+      const articlesData = await this.http
+        .get<any[]>(`${this.apiUrl}/get_articles`)
+        .toPromise();
+      this.articles = articlesData || [];
 
-        Promise.all(articlePromises).then((articlesWithSummaries) => {
-          this.articles = articlesWithSummaries;
-          this.http
-            .post<any>(`${this.apiUrl}/gen_categories`, {
-              articles: this.articles,
-            })
-            .subscribe((categoryResponse) => {
-              this.articles.forEach((article, index) => {
-                article.category = categoryResponse.categories[index].category;
-              });
-              this.categories = Array.from(
-                new Set(this.articles.map((article) => article.category))
-              );
-              this.filterArticles();
-              this.loading = false;
-            });
-        });
+      const articlePromises = this.articles.map((article) =>
+        this.http
+          .post<any>(`${this.apiUrl}/gen_summaries`, {
+            title: article.title,
+            content: article.content,
+          })
+          .toPromise()
+          .then((summaryResponse) => {
+            article.slug = summaryResponse.slug;
+            article.summary = summaryResponse.summary;
+            return article;
+          })
+      );
+
+      this.articles = await Promise.all(articlePromises);
+
+      const categoryResponse = await this.http
+        .post<any>(`${this.apiUrl}/gen_categories`, {
+          articles: this.articles,
+        })
+        .toPromise();
+
+      this.articles.forEach((article, index) => {
+        article.category = categoryResponse.categories[index].category;
       });
+
+      this.categories = Array.from(
+        new Set(this.articles.map((article) => article.category))
+      );
+    } catch (error) {
+      console.error('Error scraping articles:', error);
+    }
+  }
+
+  async loadArticles() {
+    this.loading = true;
+
+    try {
+      const blobServiceClient = await this.initializeAzureStorage();
+      const containerClient = blobServiceClient.getContainerClient('scrapes');
+
+      const blobs = containerClient.listBlobsFlat({ prefix: 'articles' });
+      let latestBlob;
+      for await (const blob of blobs) {
+        if (
+          !latestBlob ||
+          blob.properties.lastModified > latestBlob.properties.lastModified
+        ) {
+          latestBlob = blob;
+        }
+      }
+
+      if (latestBlob) {
+        const blobClient = containerClient.getBlobClient(latestBlob.name);
+        const downloadResponse = await blobClient.download();
+        const articlesData = downloadResponse.blobBody
+          ? await (await downloadResponse.blobBody).text()
+          : '';
+        this.articles = articlesData ? JSON.parse(articlesData) : [];
+      } else {
+        await this.scrapeArticles();
+      }
+    } catch (error) {
+      console.error('Error loading articles:', error);
+    }
+
+    this.filterArticles();
+    this.loading = false;
+  }
+
+  private async saveScrapedData() {
+    try {
+      const blobServiceClient = await this.initializeAzureStorage();
+      const containerClient = blobServiceClient.getContainerClient('scrapes');
+      const blobName = `articles-${new Date().toISOString()}.json`;
+      const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+      const articlesData = JSON.stringify(this.articles);
+      await blockBlobClient.upload(articlesData, articlesData.length);
+      console.log('Scraped data saved to Azure Storage.');
+    } catch (error) {
+      console.error('Error saving scraped data:', error);
+    }
+  }
+
+  async refreshData() {
+    this.loading = true;
+    await this.scrapeArticles();
+    await this.saveScrapedData();
+    this.filterArticles();
+    this.loading = false;
   }
 
   addPopoverListeners() {
